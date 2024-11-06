5c605ebb979b848cf594ddd78b22ca80f4bc4032
Malik Teague <malikisiah214@gmail.com>
2024-11-05 20:12:57 -0600

    block editor

---
+++ src/components/Editor.tsx
@@ -0,0 +1,19 @@
+"use client"; // this registers <Editor> as a Client Component
+import "@blocknote/core/fonts/inter.css";
+import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+import type { PartialBlock } from "@blocknote/core";
+
+// Our <Editor> component we can reuse later
+export default function Editor({
+  initialData,
+}: {
+  initialData: PartialBlock[];
+}) {
+  // Creates a new editor instance.
+  const editor = useCreateBlockNote({ initialContent: initialData });
+
+  // Renders the editor instance using a React component.
+  return <BlockNoteView theme={"light"} editor={editor} />;
+}
