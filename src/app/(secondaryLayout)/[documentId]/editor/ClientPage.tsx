f8237152a9b60b9d6253df6610ea3c3d09a96d7f
Malik Teague <malikisiah214@gmail.com>
2024-11-20 21:33:51 -0600

    updated loading state

--- src/app/(secondaryLayout)/[documentId]/editor/ClientPage.tsx
+++ src/app/(secondaryLayout)/[documentId]/editor/ClientPage.tsx
@@ -13,7 +13,7 @@ export default function ClientPage({
   return (
     <>
       <div>
-        <main className="py-10 lg:pl-72">
+        <main className="py lg:pl-72">
           <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <Editor initialContent={initialContent} documentId={documentId} />
           </div>
--- src/app/(secondaryLayout)/[documentId]/editor/loading.tsx
+++ src/app/(secondaryLayout)/[documentId]/editor/loading.tsx
@@ -1,9 +1,7 @@
 export default function loading() {
   return (
-    <div className="mx-auto flex max-w-7xl flex-col justify-center gap-4">
-      <div className="skeleton mx-auto h-5 w-44"></div>
-
-      <div className="skeleton h-96 w-full"></div>
+    <div className="mx-auto mb-32 mt-56 flex max-w-7xl items-center justify-center gap-4">
+      <span className="loading loading-ring loading-lg size-36"></span>
     </div>
   );
 }
